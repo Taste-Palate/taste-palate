e451dba92a7b786ca3ccc0551114943646e510df
zmjjkk98 <ysyt3199@gmail.com>
2023-11-24 20:55:51 +0900

    REFACTOR: post controller 이동
    수정,조회 등 에러처리 변경

--- back/controllers/post.js
+++ back/controllers/post.js
@@ -1,35 +1,217 @@
-exports.getMyPosts = (req, res) => {
-  console.log("내 게시글 모두 조회가 실행되었습니다.");
-  res.json("내 게시글 모두 조회가 실행되었습니다.");
-  return;
-};
+const { User, Post } = require("../models");
 
 exports.getPosts = async (req, res, next) => {
-  console.log("게시글 모두 조회가 실행되었습니다.");
-  res.json("게시글 모두 조회가 실행되었습니다.");
-  return;
+  try {
+    const post = await Post.findAll({
+      attributes: ["id", "title", "content", "imagePath", "rating", "location", "createdAt", "updatedAt", "author"],
+      include: [
+        {
+          model: User,
+          attributes: ["nick", "email"]
+        }
+      ]
+    });
+
+    if (!post) {
+      return res.status(400).json({
+        success: false,
+        message: "목록 조회 실패"
+      });
+    }
+
+    return res.json({
+      success: true,
+      message: "목록 조회 성공",
+      post
+    });
+  } catch (errMessage) {
+    console.log(errMessage);
+
+    return res.status(500).json({
+      success: false,
+      message: "예기치 못한 에러가 발생했습니다 관리자에게 문의 해주세요."
+    });
+  }
+};
+
+exports.getMyPosts = async (req, res) => {
+  try {
+    // 이것들로 유저 찾을거임
+    const user = await User.findOne({
+      where: { email: res.locals.user.email }
+    });
+
+    const post = await Post.findAll({
+      attributes: ["id", "title", "content", "imagePath", "rating", "location", "createdAt", "updatedAt", "author"],
+      include: [
+        {
+          model: User,
+          attributes: ["nick", "email"]
+        }
+      ]
+    });
+
+    if (!post) {
+      return res.status(400).json({
+        success: false,
+        message: "나의 목록 조회 실패"
+      });
+    }
+    return res.json({
+      success: true,
+      message: "나의 목록 조회 성공",
+      post
+    });
+  } catch (errMessage) {
+    console.log(errMessage);
+    return res.status(500).json({
+      success: false,
+      message: "예기치 못한 에러가 발생했습니다 관리자에게 문의 해주세요."
+    });
+  }
 };
 
 exports.getPostDetail = async (req, res, next) => {
-  console.log("게시글 상세 조회가 실행되었습니다.");
-  res.json("게시글 상세 조회가 실행되었습니다.");
-  return;
+  try {
+    const post = await Post.findOne({
+      where: { id: req.params.id },
+      attributes: ["id", "title", "content", "imagePath", "rating", "location", "createdAt", "updatedAt", "author"],
+      include: [
+        {
+          model: User,
+          attributes: ["nick", "email"]
+        }
+      ]
+    });
+
+    if (!post) {
+      return res.status(401).json({
+        success: false,
+        message: "상세 조회 실패",
+        post
+      });
+    } else {
+      return res.status(200).json({
+        success: true,
+        message: "상세 조회 성공",
+        post
+      });
+    }
+  } catch (errMessage) {
+    console.log(errMessage);
+    return res.status(500).json({
+      success: false,
+      message: "Error!"
+    });
+  }
 };
 
 exports.createPost = async (req, res, next) => {
-  console.log("게시글 추가가 실행되었습니다.");
-  res.json("게시글 추가가 실행되었습니다.");
-  return;
+  try {
+    const { title, content, imagePath, rating, location } = req.body;
+
+    // 유저 찾기
+    const user = await User.findOne({
+      where: { email: res.locals.user.email }
+    });
+
+    const post = new Post({
+      userId: User.id,
+      title,
+      content,
+      imagePath,
+      rating,
+      location
+    });
+
+    await post.save();
+
+    if (!title || !content || !imagePath || !rating || !location) {
+      // 공란이 있으면 작성 실패
+      if (title) {
+        alert("제목을 작성해주세요");
+      } else if (content) {
+        alert("내용을 작성해주세요");
+        return;
+      } else if (imagePath) {
+        alert("이미지를 삽입해주세요");
+        return;
+      } else if (rating) {
+        alert("별점을 등록해주세요");
+        return;
+      } else if (location) {
+        alert("위치를 설정해주세요");
+        return;
+      }
+    } else {
+      return res.status(200).json({
+        success: true,
+        message: "게시글 등록에 성공하였습니다."
+      });
+    }
+  } catch (errMessage) {
+    console.log(errMessage);
+    return res.status(405).json({
+      success: false,
+      message: "공란이 없게 작성해주세요"
+    });
+  }
 };
 
 exports.putPost = async (req, res, next) => {
-  console.log("게시글 수정이 실행되었습니다.");
-  res.json("게시글 수정이 실행되었습니다.");
-  return;
+  try {
+    const post = await Post.findOne({ where: { id: req.params.id } });
+    const { title, content, imagePath, rating, location } = req.body;
+
+    if (!title || !content || !imagePath || !rating || !location) {
+      return res.status(400).json({
+        message: "공란이 없게 작성해주세요."
+      });
+    }
+    if (!post) {
+      return res.status(404).json({
+        message: "게시글이 존재하지 않습니다."
+      });
+    }
+    if (post.author !== res.locals.user.id) {
+      return res.status(401).json({
+        message: "게시글을 수정할 권한이 존재하지 않습니다."
+      });
+    }
+
+    post.title = title;
+    post.content = content;
+    post.imagePath = imagePath;
+    post.rating = rating;
+    post.location = location;
+
+    await post.save();
+  } catch (error) {
+    res.status(500).json({
+      message: "오류가 발생했습니다."
+    });
+  }
 };
 
 exports.deletePost = async (req, res, next) => {
-  console.log("게시글 삭제가 실행되었습니다.");
-  res.json("게시글 삭제가 실행되었습니다.");
-  return;
+  try {
+    const post = await Post.findOne({ where: { id: req.params.id } });
+
+    if (!post) {
+      return res.status(404).json({
+        message: "게시글이 존재하지 않습니다."
+      });
+    }
+    if (post.author !== res.locals.user.id) {
+      return res.status(401).json({
+        message: "게시글을 삭제할 권한이 존재하지 않습니다."
+      });
+    }
+
+    await post.destroy();
+  } catch (error) {
+    res.status(500).json({
+      message: "오류가 발생했습니다."
+    });
+  }
 };
--- back/routes/post.js
+++ back/routes/post.js
@@ -10,210 +10,22 @@ const router = express.Router();
 
 // 게시물 모두 조회
 // http://localhost:8000/posts/getPosts
-router.get("/getPosts", async (req, res) => {
-  try {
-    const post = await Post.findAll({
-      attributes: ["id", "title", "content", "imagePath", "rating", "location", "createdAt", "updatedAt", "author"],
-      include: [
-        {
-          model: User,
-          attributes: ["nick", "email"]
-        }
-      ]
-    });
-    return res.json({
-      success: true,
-      message: "목록 조회 성공",
-      post
-    });
-  } catch (errMessage) {
-    console.log(errMessage);
-    return res.status(400).json({
-      success: false,
-      message: "목록 조회 실패"
-    });
-  }
-});
+router.get("/getPosts", getPosts, async (req, res) => {});
 
 // 내 게시물 조회
 // 게시물 모두 조회에서 + 로그인한 유저인지 검증하고, 그 유저가 등록한 게시물만 조회해주기
-router.get("/getMyPosts", verifyToken, async (req, res) => {
-  try {
-    // 이것들로 유저 찾을거임
-    const user = await User.findOne({
-      where: { email: res.locals.user.email }
-    });
-
-    console.log(user);
-
-    const post = await Post.findAll({
-      attributes: ["id", "title", "content", "imagePath", "rating", "location", "createdAt", "updatedAt", "author"],
-      include: [
-        {
-          model: User,
-          attributes: ["nick", "email"]
-        }
-      ]
-    });
-    return res.json({
-      success: true,
-      message: "나의 목록 조회 성공",
-      post
-    });
-  } catch (errMessage) {
-    console.log(errMessage);
-    return res.status(400).json({
-      success: false,
-      message: "나의 목록 조회 실패"
-    });
-  }
-});
+router.get("/getMyPosts", getMyPosts, verifyToken, async (req, res) => {});
 
 // 게시물 상세조회
-router.get("/getPosts/:id", verifyToken, async (req, res) => {
-  try {
-    const post = await Post.findOne({
-      where: { id: req.params.id },
-      attributes: ["id", "title", "content", "imagePath", "rating", "location", "createdAt", "updatedAt", "author"],
-      include: [
-        {
-          model: User,
-          attributes: ["nick", "email"]
-        }
-      ]
-    });
-
-    if (!post) {
-      return res.status(401).json({
-        success: false,
-        message: "상세 조회 실패",
-        post
-      });
-    } else {
-      return res.status(200).json({
-        success: true,
-        message: "상세 조회 성공",
-        post
-      });
-    }
-  } catch (errMessage) {
-    console.log(errMessage);
-    return res.status(500).json({
-      success: false,
-      message: "Error!"
-    });
-  }
-});
+router.get("/getPosts/:id", getPostDetail, verifyToken, async (req, res) => {});
 
 // 게시물 작성
-router.post("/writePost", verifyToken, async (req, res) => {
-  try {
-    const { title, content, imagePath, rating, location } = req.body;
+router.post("/writePost", createPost, verifyToken, async (req, res) => {});
 
-    // 유저 찾기
-    const user = await User.findOne({
-      where: { email: res.locals.user.email }
-    });
+//게시물 수정
+router.put("getPostDetail/:id", putPost, verifyToken, putPost, async (req, res) => {});
 
-    const post = new Post({
-      userId: User.id,
-      title,
-      content,
-      imagePath,
-      rating,
-      location
-    });
-
-    await post.save();
-
-    if (!title || !content || !imagePath || !rating || !location) {
-      // 공란이 있으면 작성 실패
-      if (title) {
-        alert("제목을 작성해주세요");
-      } else if (content) {
-        alert("내용을 작성해주세요");
-        return;
-      } else if (imagePath) {
-        alert("이미지를 삽입해주세요");
-        return;
-      } else if (rating) {
-        alert("별점을 등록해주세요");
-        return;
-      } else if (location) {
-        alert("위치를 설정해주세요");
-        return;
-      }
-    } else {
-      return res.status(200).json({
-        success: true,
-        message: "게시글 등록에 성공하였습니다."
-      });
-    }
-  } catch (errMessage) {
-    console.log(errMessage);
-    return res.status(405).json({
-      success: false,
-      message: "공란이 없게 작성해주세요"
-    });
-  }
-});
-
-router.put("getPostDetail/:id", verifyToken, putPost, async (req, res) => {
-  try {
-    const post = await Post.findOne({ where: { id: req.params.id } });
-    const { title, content, imagePath, rating, location } = req.body;
-
-    if (!title || !content || !imagePath || !rating || !location) {
-      return res.status(400).json({
-        message: "공란이 없게 작성해주세요."
-      });
-    }
-    if (!post) {
-      return res.status(404).json({
-        message: "게시글이 존재하지 않습니다."
-      });
-    }
-    if (post.author !== res.locals.user.id) {
-      return res.status(401).json({
-        message: "게시글을 수정할 권한이 존재하지 않습니다."
-      });
-    }
-
-    post.title = title;
-    post.content = content;
-    post.imagePath = imagePath;
-    post.rating = rating;
-    post.location = location;
-
-    await post.save();
-  } catch (error) {
-    res.status(500).json({
-      message: "오류가 발생했습니다."
-    });
-  }
-});
-
-router.delete("getPostDetail/:id", verifyToken, deletePost, async (req, res) => {
-  try {
-    const post = await Post.findOne({ where: { id: req.params.id } });
-
-    if (!post) {
-      return res.status(404).json({
-        message: "게시글이 존재하지 않습니다."
-      });
-    }
-    if (post.author !== res.locals.user.id) {
-      return res.status(401).json({
-        message: "게시글을 삭제할 권한이 존재하지 않습니다."
-      });
-    }
-
-    await post.destroy();
-  } catch (error) {
-    res.status(500).json({
-      message: "오류가 발생했습니다."
-    });
-  }
-});
+//게시물 삭제
+router.delete("getPostDetail/:id", deletePost, verifyToken, deletePost, async (req, res) => {});
 
 module.exports = router;
